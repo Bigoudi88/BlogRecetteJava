8aaa428a009f7a27a8b1f24ddab45cc54bcffb05
Isabelle B <ibert@thenuumfactory.fr>
2021-07-23 11:18:46 +0200

    Modif page recette

--- src/com/blogrecette/servlets/RecetteServlet.java
+++ src/com/blogrecette/servlets/RecetteServlet.java
@@ -1,11 +1,16 @@
 package com.blogrecette.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.blogrecette.pojos.Recette;
+import com.blogrecette.services.RecetteService;
 
 /**
  * Servlet implementation class RecetteServlet
@@ -26,7 +31,17 @@ public class RecetteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		request.getParameter("id");
+		
+		RecetteService recetteService = new RecetteService();
+		
+		
+		Recette recette = recetteService.getRecetteFromId(Integer.parseInt(request.getParameter("id")));
+		
+		request.setAttribute("recette", recette);
+
+		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/recette.jsp").forward(request, response);
 	}
 
